490be84141394d18990bf51625a37caa09ac9525
David Kelly <davekelly1224@gmail.com>
2024-04-05 12:49:58 +0200

    linted using prettier

--- survey/static/survey/loginCheck.js
+++ survey/static/survey/loginCheck.js
@@ -1,35 +1,31 @@
-document.addEventListener('DOMContentLoaded',() => {
+document.addEventListener("DOMContentLoaded", () => {
+  console.log(document.title);
 
-    console.log(document.title)
+  // disable login button initially
+  document.querySelector("#loginBtn").disabled = true;
+  // disable password field initially
+  document.querySelector("#password").disabled = true;
 
-    // disable login button initially
-    document.querySelector('#loginBtn').disabled = true
-    // disable password field initially
-    document.querySelector('#password').disabled = true
-    
-    document.querySelector('#username').onkeyup = () => {
-
-        // if username is not blank unlock password form
-        if(document.querySelector('#username').value != ''){
-            document.querySelector('#password').disabled = false
-        }else{
-            document.querySelector('#password').disabled = true
-        }
+  document.querySelector("#username").onkeyup = () => {
+    // if username is not blank unlock password form
+    if (document.querySelector("#username").value != "") {
+      document.querySelector("#password").disabled = false;
+    } else {
+      document.querySelector("#password").disabled = true;
     }
+  };
 
-    document.querySelector('#password').onkeyup = () => {
+  document.querySelector("#password").onkeyup = () => {
+    // check if form is not blank
+    if (
+      document.querySelector("#username").value != "" &&
+      document.querySelector("#password").value.length >= 8
+    ) {
+      document.querySelector("#loginBtn").disabled = false;
 
-        // check if form is not blank
-        if(document.querySelector('#username').value != '' && document.querySelector('#password').value.length >= 8 ){
-            
-            document.querySelector('#loginBtn').disabled = false
-            
-            console.log('ready to submit')
-        }else{
-
-            document.querySelector('#loginBtn').disabled = true
-            
-        }
+      console.log("ready to submit");
+    } else {
+      document.querySelector("#loginBtn").disabled = true;
     }
-    
-})
+  };
+});
--- survey/static/survey/virtualSizeSet.js
+++ survey/static/survey/virtualSizeSet.js
@@ -1,10 +1,10 @@
-// This function is to check effective vitual height and width of screen. 
-// This is implemented for mobile scaling as vh changes depending on onscreen keyboard etc. 
+// This function is to check effective vitual height and width of screen.
+// This is implemented for mobile scaling as vh changes depending on onscreen keyboard etc.
 
 let vh = window.innerHeight * 0.01;
 
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+document.documentElement.style.setProperty("--vh", `${vh}px`);
 
 let vw = window.innerWidth * 0.01;
 
-document.documentElement.style.setProperty('--vw', `${vw}px`);
+document.documentElement.style.setProperty("--vw", `${vw}px`);
